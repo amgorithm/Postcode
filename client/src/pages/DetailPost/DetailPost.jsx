a674c2a64f9cd3fe2ce03ef36d8f7a172e444173
Amal <amal.mukhtar@outlook.com>
2023-04-06 17:36:59 +0100

    Detail component css started

--- client/src/pages/DetailPost/DetailPost.jsx
+++ client/src/pages/DetailPost/DetailPost.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { getPost, removeBlog } from "../../utils/postService";
 // import useUser from "../../hooks/useUser";
+import "./DetailPost.css";
 
 function DetailPost() {
   // const { user } = useUser();
@@ -30,25 +31,40 @@ function DetailPost() {
   }
 
   return (
-    <div>
+    <div className="post-container">
       {post.length !== 0 ? (
         <article>
-          <div>
-            <h2>{post.title}</h2>
+          <div className="post-detail-title">
+            <h2 className="title">{post.title}</h2>
+          </div>
+          <div className="date-modify">
+            <h2 className="date">{post.createdAt.split("T")[0]}</h2>
+
+            <div className="modify-btns">
+              <Link to={`/post/edit/${post._id}`}>
+                <img
+                  src={require("../../images/edit-button.png")}
+                  alt="edit"
+                  className="edit"
+                />
+              </Link>
+
+              <button onClick={(e) => deleteBlog(e)} className="delete">
+                <img
+                  src={require("../../images/trash.png")}
+                  alt="trash"
+                  className="trash"
+                />
+              </button>
+            </div>
+          </div>
+          <div className="post-body">
             <p>{post.body}</p>
           </div>
         </article>
       ) : (
         <p>There doesn't seem to be any posts here yet.</p>
       )}
-
-      <div>
-        <Link to={`/post/edit/${post._id}`}>Edit</Link>
-      </div>
-
-      <div>
-        <button onClick={(e) => deleteBlog(e)}>Delete</button>
-      </div>
     </div>
   );
 }
